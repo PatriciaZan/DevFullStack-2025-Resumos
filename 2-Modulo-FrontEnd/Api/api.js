b2b0c8e69ebd05bd665a3daa84e8087c7168b7e8
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-07-08 21:37:17 -0300

    Consumindo uma API

--- 2-Modulo-FrontEnd/Api/api.js
+++ 2-Modulo-FrontEnd/Api/api.js
@@ -31,3 +31,58 @@ async function consultarCEP() {
 }
 
 btnBuscar.addEventListener('click', consultarCEP)
+
+
+// resumo
+// uma promise é um OBJ 
+// que representa uma expectatica de uma tarefa que não temos o result/conclusão dela no momento
+
+// pode ser True ou False
+// tres estados fullfild , pendente , rejeitda
+
+// existe para evitar o callback hell
+// operações assincronas -> ocorrer em paralelo com outras estruturas do código
+
+const minhaPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const sucesso = true
+        if(sucesso){
+            resolve('Dados carregados com sucesso')
+        } else{
+            reject(new Error('Falha ao carregar os dados.'))
+        }
+    }, 3000)
+})
+
+minhaPromise.then(resultado => {
+    console.log(resultado);
+}).catch(error => {
+    console.log('Erro: ', error);
+}).finally(() => {
+    console.log('Operação concluída.');
+})
+
+// async await
+// faz parecer, "respeita"
+
+const minhaPromise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const sucesso = true
+        if(sucesso){
+            resolve('Dados carregados com sucesso')
+        } else{
+            reject(new Error('Falha ao carregar os dados.'))
+        }
+    }, 3000)
+
+    console.log('Testeee');
+    
+})
+
+minhaPromise2.then(resultado => {
+    console.log(resultado);
+}).catch(error => {
+    console.log('Erro: ', error);
+}).finally(() => {
+    console.log('Operação concluída.');
+})
