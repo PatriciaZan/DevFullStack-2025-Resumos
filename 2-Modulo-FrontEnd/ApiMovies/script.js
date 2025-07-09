09bd4ab03fab556552a7088f16171f224427230c
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-07-09 16:10:47 -0300

    Reoslução do uso da API tmdb

---
+++ 2-Modulo-FrontEnd/ApiMovies/script.js
@@ -0,0 +1,43 @@
+
+const moviesContainer = document.getElementById('container')
+
+//
+const apiURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc`
+
+async function pegarFilmes(){
+    
+
+    try{
+        const response = await fetch(apiURL)
+        const data = await response.json()
+        console.log(data);
+
+        data.results.forEach(media => {
+            const movieCard = createCards(media)
+            moviesContainer.appendChild(movieCard)
+        });
+       
+    } catch(error){
+        console.error('Erro: ', error)
+    }
+
+}
+
+function createCards(media){
+    const {title, name, backdrop_path} = media
+
+    const movieCard = document.createElement('div')
+    movieCard.classList.add('movie_item')
+
+    movieCard.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" class="movie_img_rounded">
+        <div class="title">${title || name}</div>
+    `;
+
+    return movieCard
+}
+
+
+pegarFilmes()
+
+
