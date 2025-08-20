53abe518946553b85ee07d6faf31b6274927f33d
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-08-19 22:11:04 -0300

    testes e bugs

---
+++ 3-React/tests/src/components/Counter.test.js
@@ -0,0 +1,17 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import Counter from "./Counter";
+
+import "@testing-library/jest-dom";
+
+describe("Counter", () => {
+  it("Deve incrementar o contador quando o botão for clicado.", () => {
+    render(<Counter />);
+
+    const countElement = screen.getByTestId("count");
+    const buttonElement = screen.getByText("Incrementar");
+
+    fireEvent.click(buttonElement);
+    expect(countElement).toHaveTextContent("1");
+  });
+});
