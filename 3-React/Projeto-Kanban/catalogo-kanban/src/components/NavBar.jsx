959dda7d5a140e4d0c39163c618d03534660d07e
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-08-26 22:11:20 -0300

    Projeto-Kanban

--- 3-React/Projeto-Kanban/catalogo-kanban/src/components/NavBar.jsx
+++ 3-React/Projeto-Kanban/catalogo-kanban/src/components/NavBar.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import "../styles/components/navbar.css";
 
 export default function NavBar() {
+  const [link, setLink] = useState(true);
+
+  const handleActive = (value) => {
+    setLink(value);
+    console.log(link);
+  };
+
   return (
-    <div>
-      <nav>
+    <div className="navBar-container">
+      <nav className="navBar-Nav">
         <h1>Title</h1>
-        <div>
-          <Link to="/catalogo">Catálogo</Link>
-          <Link to="/kanban">Kaban</Link>
+
+        <div className="navBar-toggle">
+          <Link
+            onClick={() => handleActive(true)}
+            className={link === true ? "navBar-Link_catalogo" : ""}
+            to="/catalogo"
+          >
+            Catálogo
+          </Link>
+          <Link
+            onClick={() => handleActive(false)}
+            className={link === false ? "navBar-Link_kanban" : ""}
+            to="/kanban"
+          >
+            Kaban
+          </Link>
         </div>
 
         <div>
