d16d5da8e56da15a9441b6e80da44278b96aeb97
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-08-30 11:49:43 -0300

    Introducao a Java

---
+++ 4-Java/Main.java
@@ -0,0 +1,264 @@
+//import Scanner
+public class Main {
+    public static void main(String[] args){
+//        int intVar = 2147483647;
+//        long longVar = 9223372036854775807L;
+//        byte byteVar = 127;
+//        short shortVar = 32767;
+//
+//        double doubleVar = 637216378678216.0;
+//        float floatVar = 3918273891738912.0f;
+//
+//        char charVat = 'A';
+//
+//        boolean boolean = true;
+//
+//        System.out.println("intVal = " + intVar);
+//        //System.out.println("intVal = " + intVar);
+//        System.out.printf("%.2f \n", floatVar);
+
+//        String name = "Patricia";
+//
+//        int a, b, c;
+//        a= 5;
+//        b= 7;
+//        c= 3;
+//
+//        area = (double) (a+b)/2 * c;
+//        System.out.println(area);
+
+        // Leitura
+//        System.out = new Scanner(System.in);
+//
+//        int numero = input.nextInt();
+//        System.out.println('Numero ' + numero)
+
+        // EXERCICIO 01.
+        // Conversão de Celsisus para fahrenheit
+        //int celcius = 21;
+//        Scanner input = new Scanner(System.in);
+//
+//        double celcius = input.nextDouble()
+//        double fahrenheit = (celcius * 9.0/5.0) + 32;
+//        System.out.println('In Fehrenheit: '+ fahrenheit);
+
+        //Locale(language: "pt", country: "BR")
+        //locale us = Locale.US;
+        //Locale atual = Locale.getDefault();
+
+        // EXERCICIO 02
+        // Verificação de Maior de Idade
+//        int idade = 19;
+//        if(idade >= 18){
+//            System.out.println("Você é maior de idade!");
+//        }else{
+//            System.out.println("Você não é maior de idade!")
+//        }
+
+        // EXERCICIO 03
+        //Par ou impar
+//        int number03 = 10;
+//        if(number03 % 2 == 0){
+//            System.out.println(number03 + "é um nuemro par.")
+//        } else{
+//            System.out.println(number03 + " é um número ímpar.")
+//        }
+
+        // EXERCICIO 04
+        // Dia da semana usando SWITCH-CASE
+//        System.out.println("Digite o número da semana: ")
+//        int dia= input.next();
+//
+//        switch (int){
+//            case 1:
+//                System.out.println("Segunda-Feira");
+//                break;
+//
+//            case 2:
+//                System.out.println("Terça-Feira");
+//                break;
+//
+//            case 3:
+//                System.out.println("Quarta-Feira");
+//                break;
+//
+//            case 4:
+//                System.out.println("Quinta-Feira");
+//                break;
+//
+//            case 5:
+//                System.out.println("Sexta-Feira");
+//                break;
+//
+//            case 6:
+//                System.out.println("SabadoUU");
+//                break;
+//
+//            case 7:
+//                System.out.println("DomingouUU");
+//                break;
+//
+//            default:
+//                System.out.println("Dia invalido!");
+//                break;
+//        }
+
+        // EXERCICO 05
+        // Criar uma calculadora
+//        Scanner calc = new Scanner(System.in);
+//        System.out.println("Digite um numero: ");
+//        int num1 = calc.nextInt();
+//        System.out.println("Digite um operador: ");
+//        char oper = calc.next().charAt(0);
+//        System.out.println("Digite um numero: ");
+//        int num2 = calc.nextInt();
+//
+//        if (oper == '-') {
+//            int res =  num1 - num2;
+//            System.out.println("Resultado: " + res);
+//        } else if (oper == '+') {
+//            int res =  num1 + num2;
+//            System.out.println("Resultado: " + res);
+//        } else if (oper == '*') {
+//            int res =  num1 * num2;
+//            System.out.println("Resultado: " + res);
+//        } else if (oper == '/') {
+//            int res =  num1 / num2;
+//            System.out.println("Resultado: " + res);
+//        } else {
+//            System.out.println("Invalid operador");
+//        }
+
+        // EXERCICIO 06
+        //Contar 1-10 (for)
+
+//        for(let i = 0; i <= 10; i++){
+//            System.out.println(i)
+//        }
+//
+//        int i=0;
+//        while(i<= 10){
+//            System.out.println("Valor: " + i);
+//            i++
+//        }
+//        int i2 = 1
+//        do{
+//            System.out.prinln(i2++);
+//
+//        }while(i <=10)
+
+//        String text = "Grêmio é maior que aquele time";
+//        int comprimento = texto.length(); // 30
+//        System.out.println(comprimento);
+//
+//        String maisculo = texto.toUpperCase();
+//        System.out.println(maisculo);
+//
+//        boolean contem = texto.contains("Grêmio");
+//        System.out.println(contem);
+//
+//        String substituto = texto.replace("time", "Grupo");
+//        System.out.println(substituto);
+//
+//        String sobstring = texto.substring(0,6);
+//        System.out.println(sobstring);
+
+
+//        double num = 50.30;
+//        double arrendodado = Math.round(num);
+//        System.out.println(arrendodado);
+//
+//        System.out.println(Math.ceil(num));
+//        System.out.println(Math.floor(num));
+//        System.out.println(Math.round(num));
+//        System.out.println(Math.sqrt(num));
+//        System.out.println(Math.abs(num));
+//        System.out.println(Math.random(num));
+
+//        int [] vetor = new int[5];
+//        vetor[0] = 1;
+//        vetor[1] = 2;
+//        vetor[2] = 3;
+//        vetor[3] = 4;
+//        vetor[4] = 5;
+//
+//        System.out.println('Elementos do Array: ');
+//        for (int i = 0; i < veotr.length; i++){
+//            System.out.println(vetor[i]);
+//        }
+//
+//        for(int i = 0; i < vetor.length; i++){
+//            soma += vetor[i]
+//        }
+//
+//        System.out.println(soma);
+
+        // EXERCICIO 07
+        // encontrar o maior numero
+//        int max = vetor[0];
+//
+//        for(int i = 1; i < vetor.length; i++){
+//            if(vetor[i] > max){
+//                max = vetor[i];
+//            }
+//        }
+//        System.out.println(max);
+
+        //EXERCICIO 08
+        // Adicione um elemento ao Array
+//        int [] vetor = new int[2];
+//        vetor[0] = 1;
+//        vetor[1] = 2;
+//
+//        int [] novovetor = new int[vetor.length];
+//        for(int i = 0; i< vetor.length; i++){
+//            novovetor[i] = vetor[i]
+//        }
+//        novovetor[vetor.length] = 1;
+
+        int [][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        for(int i = 0; i <matrix.length; i++){
+            for(int j = 0; j <matrix.length; j++){
+                System.out.println(matrix[i][j]);
+            }
+        }
+
+        // EXERCICIO 09
+        // Sama das diagonais da matriz
+
+        int diagonal = 0;
+        int diagonal2 = 0;
+
+        for(int i = 0; i <matrix.length; i++){
+            diagonal += matrix[i][i];
+            diagonal2 += matrix[i][matrix.length - 1 -i]
+            }
+        }
+
+        System.out.println(diagonal);
+        System.out.println(diagonal2);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
